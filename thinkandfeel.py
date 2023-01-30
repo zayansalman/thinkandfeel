4aa4864c99ef10593c1499ee207a322f37fd97b7
Zayan <zayan_k@icloud.com>
2023-01-30 17:19:23 +0000

    plot plays for top 5 countries with matplotlib scatter

--- thinkandfeel.py
+++ thinkandfeel.py
@@ -4,12 +4,14 @@ from geopy.geocoders import Nominatim
 import plotly.express as px
 import country_converter as coco
 import numpy as np
+import matplotlib.pyplot as plt
 cc = coco.CountryConverter()
 
 geolocator = Nominatim(user_agent="geoapiExercises")
 
 tnf_df = pd.read_excel('thinkandfeel.xlsx')
 tnf_df['iso_a3'] = cc.pandas_convert(series=tnf_df['Country of Sale'], to='ISO3')
+tnf_df['log_plays'] = np.log10(tnf_df['Quantity'])
 # get most recent Reporting Date from tnf_df in a new variable
 reporting_date = tnf_df['Reporting Date'].max()
 # get total Quantity from tnf_df in a new variable
@@ -34,7 +36,24 @@ for index, row in tnf_ce_df.iterrows():
 # Add the coordinates as new columns in the dataframe
 tnf_ce_df['latitude'] = [x[0] for x in coordinates]
 tnf_ce_df['longitude'] = [x[1] for x in coordinates]
-tnf_ce_df['log_plays'] = np.log10(tnf_ce_df['Quantity'])
+
+# top 10 countries by plays
+top10_countries = tnf_ce_df.head()['iso_a3'].values
+tnf_df.sort_values('Quantity', ascending=False, inplace=True)
+top10_df = tnf_df[tnf_df['iso_a3'].isin(top10_countries)]
+top10_df = top10_df.set_index('Reporting Date')
+top10_df = top10_df.sort_index(ascending=True)
+
+countries = top10_df['iso_a3'].unique()
+
+for country in countries:
+    plays = top10_df[top10_df['iso_a3'] == country]
+    plt.scatter(plays.index, np.log(plays['Quantity']), label=country)
+
+plt.title('Top 5 Countries')
+plt.xlabel('Reporting Date')
+plt.ylabel('Log scaled Plays')
+plt.legend()
 
 fig = px.choropleth(tnf_ce_df, locations='iso_a3',
                     locationmode='ISO-3',
@@ -53,6 +72,12 @@ fig.update_layout(autosize=True, geo=dict(bgcolor='rgba(0,0,0,0)'), showlegend=F
 st.set_page_config(page_title="think and feel", page_icon=None, layout="centered")
 st.title("think and feel")
 st.write('Analytics as of ' + str(reporting_date.date()))
-st.metric(label='Total Plays:', value=str(total_quantity))
-st.metric(label='Total Earnings:', value='$' + str(round(total_earnings)))
+col1, col2 = st.columns(2)
+
+with col1:
+    st.metric(label='Total Plays:', value=str(total_quantity))
+    st.metric(label='Total Earnings:', value='$' + str(round(total_earnings)))
+with col2:
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot()
 st.plotly_chart(fig)
